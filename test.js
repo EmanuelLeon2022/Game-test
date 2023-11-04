4828e5bc42d12445de823aa52533c725bc1f98be
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-11-04 19:09:40 -0400

    more code

--- test.js
+++ test.js
@@ -11,9 +11,12 @@ class pilot{
 }
 //Instantiated new pilot
 const USSA = new pilot('USS ASSEMBLY', 20,  5)
+for(let i=0; i<=10; i=i+1){
+    console.log(i)
+}
 
 //Defined reesult actions
-if (USSA.hull<= 0){
+if(USSA.hull<= 0){
     console.log("YOU DIED!!!")
 }else if(USSA.hull>=0){
     console.log("KEEP FIGHTING!!!")
@@ -34,16 +37,21 @@ class enemy{
 //Created new Enemies
 
 const mars = new enemy('Mars', 5, 5)
+// const griffin
+// const chimera
+// const typhon
+// const 
 
-const retreat = document.querySelector('button')
+const retreat = document.querySelector('#retreat')
 retreat.addEventListener('click',() =>{
-    // console.log('retreat')
-    // player1.ugotShot
+    console.log('retreat')
     const result = document.querySelector('h1')
     result.textContent = "GAME OVER"
 })
 
 const Mars = document.querySelector('#mars')
 Mars.addEventListener('click',() =>{
+    mars.takeaHit
     console.log(mars)
+    USSA.takeHit
 })
