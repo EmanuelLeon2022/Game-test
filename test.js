f3f14cc33e9b6b54a577fb8487a600682d13bbae
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-11-05 01:00:18 -0400

    currently still adding more math

--- test.js
+++ test.js
@@ -4,15 +4,16 @@ class pilot{
         this.hull = hull;
         this.firepower = fire;
     }
-    takeHit(enemy){
-        this.enemy.pull(enemy)
-        pilot.hull-=enemy.efire
+    takeHit(){
+        // this.enemy.push(enemy)
+        // pilot.hull-=enemy.efire
+        this.hull--
     }
 }
 //Instantiated new pilot
-const USSA = new pilot('USS ASSEMBLY', 20,  5)
-for(let i=0; i<=10; i=i+1){
-    console.log(i)
+const USSA = new pilot('USS ASSEMBLY', 20, 5)
+for (let i=0; i<5; i++){
+    USSA.takeHit()
 }
 
 //Defined reesult actions
@@ -22,36 +23,56 @@ if(USSA.hull<= 0){
     console.log("KEEP FIGHTING!!!")
 }
 
+
 class enemy{
-    constructor(ename, ehull, efire){
+    constructor(ename, ehull, efire, accuracy){
         this.ename = ename
-        this.ehull = ehull
-        this.efirepower = efire
+        this.ehull = Math.floor(Math.random()*4) + 3;
+        this.efirepower = (Math.floor(Math.random() * 3) + 2);
+        this.accuracy = (Math.floor(Math.random() * 3) + 6) /10;
+        this.bigshot= () =>{
+            let ranNum = Math.random();
+            console.log(`Accuracy of enemy is ${ranNum}`);
+            if(ranNum < this.accuracy){
+                console.log(`You have been hit`);
+                USSA.hull = USSA.hull - this.efirepower;
+                console.log(`USSA has ${USSA.hull} HP`);
+            }else{
+                console.log(`Evavsive Manuvers Engaged`);
+            }
+        }
     }
-    takeaHit(pilot){
-        this.pilot.push(pilot)
-        enemy.ehull-=pilot.fire
+    takeaHit(USSA){
+        this.USSA.push(USSA)
+        this.ehull--
     }
 }
 
-//Created new Enemies
+const setRandomNum = (min,max) =>{
+    let random =
+    Math.floor(Math.random( max - min))
+}
 
-const mars = new enemy('Mars', 5, 5)
-// const griffin
-// const chimera
-// const typhon
-// const 
+// const mars = new enemy('Mars', setRandomNum(6,3), setRandomNum(4,2))
+let harpy = new enemy('Harpy', setRandomNum(3,6), setRandomNum(2,4))
+let griffin = new enemy('Griffin', setRandomNum(3,6), setRandomNum(2,4))
+let chimera = new enemy('Chimera', setRandomNum(3,6), setRandomNum(2,4))
+let typhon = new enemy('Typhon', setRandomNum(3,6), setRandomNum(2,4))
+let serberus = new enemy('Serberus', setRandomNum(3,6), setRandomNum(2,4))
+let mars = new enemy('Mars', setRandomNum(3,6), setRandomNum(2,4))
 
-const retreat = document.querySelector('#retreat')
-retreat.addEventListener('click',() =>{
-    console.log('retreat')
-    const result = document.querySelector('h1')
-    result.textContent = "GAME OVER"
+//FIGHT BUTTON
+const FIGHT = document.querySelector('#FIGHT')
+FIGHT.addEventListener('on click', () => {
+    USSA.takeHit
+    const enemyHealth = document.querySelector('#stats')
+    enemyHealth.textContent = `USS Assembly ${USSA.hull} HP`
+    const stats =document.querySelector('#STATUS')
+    stats.textContent = `ENEMY ${enemy.ehull} HP`
 })
 
-const Mars = document.querySelector('#mars')
-Mars.addEventListener('click',() =>{
-    mars.takeaHit
-    console.log(mars)
-    USSA.takeHit
+const RETREAT = document.querySelector('#RETREAT')
+RETREAT.addEventListener('click',() =>{
+    const stats =document.querySelector('#STATUS')
+    stats.textContent = `GAME OVER`
 })
